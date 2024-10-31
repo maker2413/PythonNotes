59d398359e63d920ed677a4db99e9c29d27f0356
maker2413 <maker2413@gmail.com>
2024-10-31 18:33:52 -0500

    added information on modulo

--- coreConcepts/core-concepts.py
+++ coreConcepts/core-concepts.py
@@ -7,3 +7,11 @@ print("variable \"a\" has a type of:", type(a))
 x = 1+2j
 
 print("variable \"x\" has a type of:", type(x))
+
+print("addition:", 10+25)
+print("subtraction:", 2-9)
+print("multiplication:", 4*24)
+print("division:", 72/6)
+print("exponents:", 2**8)
+
+print("The remainder (mod) of 19 and 5 is:", 19%5)
