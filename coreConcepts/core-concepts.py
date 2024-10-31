200b0c12af6b8d3962349eab7082a06a8d04e2f9
maker2413 <maker2413@gmail.com>
2024-10-31 17:15:43 -0500

    added some very basic notes just to begin with

---
+++ coreConcepts/core-concepts.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+a = 248.1231238
+
+# You can escape characters with \ so they get printed
+print("variable \"a\" has a type of:", type(a))
+
+x = 1+2j
+
+print("variable \"x\" has a type of:", type(x))
